e96d428f899f5bc1d5d6c2b5d8e614db77d21b24
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-02-22 20:22:44 -0700

    slight changes

--- lib/structure.rb
+++ lib/structure.rb
@@ -1,14 +1,23 @@
+
 class Structure
   attr_accessor :path
 
   def initialize(path)
-    @path = path
-    create_outer_structure
+    create_outer_structure(path)
   end
 
   def create_outer_structure(path)
+    # FileUtils::mkdir_p "#{path}"
     FileUtils::mkdir_p "#{path}"
   end
+
+  # def create_inner_structure
+  #   FileUtils::mkdir_p '/source/css'
+  # end
+
+  # File.write to file
+
+
 
   #create outer file structure
   # require 'fileutils'
--- test/structure_test.rb
+++ test/structure_test.rb
@@ -1,12 +1,17 @@
-require './bin/structure'
+require'./bin/structure'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class StructureTest < Minitest::Test
 
   def test_it_creates_folders
-    folder = Structure.new('~/HydeTest')
-    assert File.directory?('~/HydeTest')
+    folder = Structure.new(Dir.home + '/HydeTest')
+    assert File.directory?(Dir.home + '/HydeTest')
+  end
+
+  def test_it_creates_subfolders_in_the_directory
+    Structure.new(Dir.home + '/HydeTest/source/css')
+    assert File.directory? (Dir.home + 'HydeTest/source/css')
   end
 
 end
