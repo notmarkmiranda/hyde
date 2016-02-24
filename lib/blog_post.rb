6c3ceb7a0e8988112d83031b502870d340ea1dd1
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-02-24 14:21:03 -0700

    Iteration 2 finished

--- lib/blog_post.rb
+++ lib/blog_post.rb
@@ -4,8 +4,8 @@ class BlogPost
   def initialize(path, file_name)
     path = "/#{path}" if path[0] != "/"
     path.split("/")[1] == "Users" ? @home = path : @home = __dir__ + path
-    file_extension = file_name.split.join("-").downcase
+    file_extension = file_name.join("-").downcase
     date = Date.today.strftime("%Y-%m-%d")
-    File.write(@home + '/source/posts/' + date + '-' + file_extension + '.md', "# #{file_name}")
+    File.write(@home + '/source/posts/' + date + '-' + file_extension + '.md', "# #{file_name.join(" ")}\n\nYour content here")
   end
 end
--- test/blog_post_test.rb
+++ test/blog_post_test.rb
@@ -6,7 +6,7 @@ class BlogPostTest < Minitest::Test
   def test_it_creates_an_additional_post_file_in_source
     folder = Structure.new('/Hyde_output')
     date = Date.today.strftime("%Y-%m-%d")
-    folder_two = BlogPost.new('/Hyde_output', 'My Post')
+    folder_two = BlogPost.new('/Hyde_output', ['My', 'Post'])
     assert File.file?('./lib/Hyde_output/source/posts/' + date + '-my-post.md')
     FileUtils.rm_rf('./lib/Hyde_output')
   end
