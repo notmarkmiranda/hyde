ab4ea0e7a2231f23fea3d14362ca4ac38dd5a2cb
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-02-24 14:25:55 -0700

    Iteration 1 really finished

--- test/blog_post_test.rb
+++ test/blog_post_test.rb
@@ -11,4 +11,12 @@ class BlogPostTest < Minitest::Test
     FileUtils.rm_rf('./lib/Hyde_output')
   end
 
+  def test_it_writes_file_name_in_md_file
+    folder = Structure.new('/Hyde_output')
+    date = Date.today.strftime("%Y-%m-%d")
+    folder_two = BlogPost.new('/Hyde_output', ['My', 'Post'])
+    assert_equal "# My Post\n\nYour content here", File.read('./lib/Hyde_output/source/posts/'+ date + '-my-post.md')
+    FileUtils.rm_rf('./lib/Hyde_output')
+
+  end
 end
