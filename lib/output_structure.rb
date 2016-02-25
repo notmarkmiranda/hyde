35efd934e832577dd77b4133f19a8474a7205cb5
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-02-25 08:16:31 -0700

    refactoring continues

--- lib/output_structure.rb
+++ lib/output_structure.rb
@@ -19,68 +19,66 @@ class OutputStructure
     FileUtils.cp_r(@home + '/source/.', @home + '/_output/')
   end
 
-  def convert_files(folder)
-    folder = @folder
-    md_files = Dir.entries(@home + '/_output/' + @folder).find_all do |filename|
-      filename.split(".")[-1] == "md"  || filename.split(".")[-1] == "markdown"
-    end
-    md_files.each do |file|
-      convert(file)
-    end
-  end
-
-  def convert(file)
-    files_md = File.read(@home + '/_output/' + @folder + file)
-    html_content = Kramdown::Document.new(files_md).to_html
-    erb_template = File.read(@home + '/source/layouts/default.html.erb')
-    erb_formatted = ERB.new(erb_template).result(binding)
-    File.write(@home + '/_output/' + @folder + file, erb_formatted)
-    File.rename(@home + '/_output/' + @folder + file, @home + '/_output/'+ @folder + file.split(".")[0] +'.html')
-  end
-
-  def convert_md
-    convert_files('pages/')
-    convert_files('posts/')
-    convert_files('') # for index.md/ index.html
-  end
+  # def convert_files(folder)
+  #   @folder = folder
+  #   md_files = Dir.entries(@home + '/_output' + @folder).find_all do |filename|
+  #     filename.split(".")[-1] == "md"  || filename.split(".")[-1] == "markdown"
+  #   end
+  #   md_files.each do |file|
+  #     convert(file)
+  #   end
+  # end
+  #
+  # def convert(file)
+  #   files_md = File.read(@home + '/_output' + @folder + file)
+  #   html_content = Kramdown::Document.new(files_md).to_html
+  #   erb_template = File.read(@home + '/source/layouts/default.html.erb')
+  #   erb_formatted = ERB.new(erb_template).result(binding)
+  #   File.write(@home + '/_output' + @folder + file, erb_formatted)
+  #   File.rename(@home + '/_output' + @folder + file, @home + '/_output'+ @folder + file.split(".")[0] +'.html')
+  # end
+  #
+  # def convert_md
+  #   convert_files('/pages/')
+  #   convert_files('/posts/')
+  #   convert_files('/') # for index.md index.html
+  # end
 
 #new method end
 
-
-    # index_md = File.read(@home + '/_output/index.md')
-    # html_content = Kramdown::Document.new(index_md).to_html
-    # erb_template = File.read(@home + '/source/layouts/default.html.erb')
-    # erb_formatted = ERB.new(erb_template).result(binding)
-    # File.write(@home + '/_output/index.md', erb_formatted)
-    # File.rename(@home + '/_output/index.md', @home + '/_output/index.html')
+  def convert_md
+    index_md = File.read(@home + '/_output/index.md')
+    html_content = Kramdown::Document.new(index_md).to_html
+    erb_template = File.read(@home + '/source/layouts/default.html.erb')
+    erb_formatted = ERB.new(erb_template).result(binding)
+    File.write(@home + '/_output/index.md', erb_formatted)
+    File.rename(@home + '/_output/index.md', @home + '/_output/index.html')
 
 # REFACTOR THESE TWO INTO ONE METHOD
 
-    # md_pages = Dir.entries(@home + '/_output/pages').find_all do |filename|
-    #   filename.split(".")[-1] == "md"  || filename.split(".")[-1] == "markdown"
-    # end
-    # md_pages.each do |file|
-    #   pages_md = File.read(@home + '/_output/pages/' + file)
-    #   html_content = Kramdown::Document.new(pages_md).to_html
-    #   erb_template = File.read(@home + '/source/layouts/default.html.erb')
-    #   erb_formatted = ERB.new(erb_template).result(binding)
-    #   File.write(@home + '/_output/pages/' + file, erb_formatted)
-    #   File.rename(@home + '/_output/pages/' + file, @home + '/_output/pages/' + file.split(".")[0] +'.html')
-    # end
-    #
-    # md_posts = Dir.entries(@home + '/_output/posts').find_all do |filename|
-    #   filename.split(".")[-1] == "md"  || filename.split(".")[-1] == "markdown"
-    # end
-    # md_posts.each do |file|
-    #   posts_md = File.read(@home + '/_output/posts/' + file)
-    #   html_content = Kramdown::Document.new(posts_md).to_html
-    #   erb_template = File.read(@home + '/source/layouts/default.html.erb')
-    #   erb_formatted = ERB.new(erb_template).result(binding)
-    #   File.write(@home + '/_output/posts/' + file, erb_formatted)
-    #   File.rename(@home + '/_output/posts/' + file, @home + '/_output/posts/' + file.split(".")[0] +'.html')
-    #
-    # end
-# REFACTOR THESE TWO INTO ONE METHOD
+    md_pages = Dir.entries(@home + '/_output/pages').find_all do |filename|
+      filename.split(".")[-1] == "md"  || filename.split(".")[-1] == "markdown"
+    end
+    md_pages.each do |file|
+      pages_md = File.read(@home + '/_output/pages/' + file)
+      html_content = Kramdown::Document.new(pages_md).to_html
+      erb_template = File.read(@home + '/source/layouts/default.html.erb')
+      erb_formatted = ERB.new(erb_template).result(binding)
+      File.write(@home + '/_output/pages/' + file, erb_formatted)
+      File.rename(@home + '/_output/pages/' + file, @home + '/_output/pages/' + file.split(".")[0] +'.html')
+    end
 
-  # end
+    md_posts = Dir.entries(@home + '/_output/posts').find_all do |filename|
+      filename.split(".")[-1] == "md"  || filename.split(".")[-1] == "markdown"
+    end
+    md_posts.each do |file|
+      posts_md = File.read(@home + '/_output/posts/' + file)
+      html_content = Kramdown::Document.new(posts_md).to_html
+      erb_template = File.read(@home + '/source/layouts/default.html.erb')
+      erb_formatted = ERB.new(erb_template).result(binding)
+      File.write(@home + '/_output/posts/' + file, erb_formatted)
+      File.rename(@home + '/_output/posts/' + file, @home + '/_output/posts/' + file.split(".")[0] +'.html')
+    end
+# REFACTOR THESE TWO INTO ONE METHOD
+  end
 end
