289ea9e32d250b976d7383e1951fd6bc376a8589
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-02-25 13:59:29 -0700

    tests on iteration 4 complete

---
+++ test/error_check_test.rb
@@ -0,0 +1,17 @@
+require './lib/error_check'
+require './lib/structure'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class ErrorCheckTest < Minitest::Test
+
+  def test_it_adds_a_slash_if_none_is_given
+    path = "blog"
+    assert_equal "/blog", ErrorCheck.path_check(path)[-5..-1]
+  end
+
+  def test_it_can_define_path_in_home_directory
+    path = "~/blog"
+    assert_equal "Users", ErrorCheck.path_check(path).split("/")[1]
+  end
+end
