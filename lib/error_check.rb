a0d8a75599ce14d86cc994ac68c6f9079a4cb0b4
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-02-25 16:50:14 -0700

    cleaned up more

--- lib/error_check.rb
+++ lib/error_check.rb
@@ -7,9 +7,4 @@ class ErrorCheck
     path = "/#{path}" if path[0] != "/"
     path.split("/")[1] == "Users" ? @home = path : @home = __dir__ + path
   end
-
-  def self.new_path_check(path)
-    path = "/#{path}" if path[0] != "/"
-    path.split("/")[1] == "Users" ? @home = path : @home = __dir__ + path
-  end
 end
